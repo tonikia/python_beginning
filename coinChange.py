4a890b65eefcdbd58d49180f2697d2cb4900d5bd
Toni JUNG <68240051+tonikia@users.noreply.github.com>
2021-05-09 16:12:43 +0900

    excercise

---
+++ coinChange.py
@@ -0,0 +1,24 @@
+## 변수선언부분
+
+money, c500, c100, c50, c10 = 0, 0, 0, 0, 0
+
+## 메인 코드 부분
+money = int(input("교환할 돈은 얼마입니까?"))
+
+c500 = money // 500
+money %= 500
+
+c100 = money // 100
+money %= 100
+
+c50 = money // 50
+money %= 50
+
+c10 = money // 10
+money %= 10
+
+print("\n 오백원짜리 ==> %d개" % c500)
+print(" 백원짜리 ==> %d 개 " %c100)
+print("오십원짜리 ==> %d 개 " %c50)
+print(" 십원짜리 ==> %d개" %c10)
+print(" 바꾸지 못한 잔돈 ==> %d원 \n" %money) 
--- inputoutput.py
+++ inputoutput.py
@@ -3,4 +3,3 @@ print(name + "님 반갑습니다")
 
 age = int(input(name +"님, 나이가 어떻게 되시나요?")) + 10
 print("10년 후면 " + str(age) + "살이 되시는 군요!")
-
